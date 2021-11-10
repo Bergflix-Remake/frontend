c5989f79017db2efbfde528aaf3427976efd0f0a
Anniken <61291253+AnnikenYT@users.noreply.github.com>
2021-11-10 21:29:49 +0100

    Update main.ts
    Removed vue-plyr for now

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import VuePlyr from "vue-plyr";
+// import VuePlyr from "vue-plyr";
 import "vue-plyr/dist/vue-plyr.css";
 import router from "./router/router";
 
