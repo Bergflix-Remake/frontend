b6afb5ef0a15cbb0e6d130bb80ab9431b9103ca5
AnnikenYT <anniken@mooonshine.net>
2022-04-14 10:59:20 +0000

    fix: 🐛 fix poster [skip ci]
    Text on posters is now centered

--- src/components/Common/Poster.vue
+++ src/components/Common/Poster.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="container" class="flex flex-col items-center justify-center transition-all bg-center bg-cover rounded-xl aspect-video hover:border-4 hover:h-64 h-60 group" :style="{ 'background-image': `url(${image})`}">
     <div class="flex flex-col items-center justify-center w-full h-full transition-all bg-gray-900 opacity-0 group-hover:opacity-100 bg-opacity-80 rounded-xl">
-        <h1 class="text-xl font-bold">{{name}}</h1>
+        <h1 class="text-xl font-bold text-center">{{name}}</h1>
         <p class="text-gray-500">{{subtitle}}</p>
         <Button type="button" :to="link" :icon="linkIcon">
             {{linkText}}
