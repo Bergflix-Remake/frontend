0d86362b2e182ff263fce4cad0a793a7ff22f2e3
AnnikenYT <anniken@mooonshine.net>
2022-07-24 15:54:03 +0000

    feat: ✨ Replaced Home Placeholders
    DEVELOP-13

--- src/stories/pages/Home/Home.vue
+++ src/stories/pages/Home/Home.vue
@@ -2,41 +2,31 @@
     <div class="flex flex-col bg-black">
         <Hero />
         <section class="p-4">
-            <Title class="mb-2"><Logo :long="true" class="!text-5xl">Originals</Logo></Title>
+            <Title class="mb-2">
+                <Logo :long="true" class="!text-5xl">Originals</Logo>
+            </Title>
             <ScrollableRow>
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
+                <Poster v-if="originals.isLoading" loading />
+                <Poster v-else-if="originals.isSuccess" v-for="movie in originals.data" :key="movie.id!"
+                    :image="movie.attributes?.thumbnail.data?.attributes?.url"
+                    :original="movie.attributes?.original!" />
+                <Poster v-if="originals.isError" error />
             </ScrollableRow>
             <Title class="mb-2">Filme</Title>
             <ScrollableRow>
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
+                <Poster v-if="videos.isLoading" loading />
+                <Poster v-else-if="videos.isSuccess" v-for="movie in videos.data" :key="movie.id!"
+                    :image="movie.attributes?.thumbnail.data?.attributes?.url"
+                    :original="movie.attributes?.original!" />
+                <Poster v-if="videos.isError" error />
             </ScrollableRow>
             <Title class="mb-2">Serien</Title>
             <ScrollableRow>
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
-                <Poster :loading="true" />
+                <Poster v-if="series.isLoading" loading />
+                <Poster v-else-if="videos.isSuccess" v-for="serie in series.data" :key="serie.id!"
+                    :image="serie.attributes?.thumbnail.data?.attributes?.url"
+                    :original="serie.attributes?.original!" />
+                <Poster v-if="series.isError" error />
             </ScrollableRow>
         </section>
     </div>
@@ -48,6 +38,41 @@ import ScrollableRow from '../../cells/ScrollableRow.vue';
 import Poster from '../../molecules/Poster.vue';
 import Title from '../../atoms/Title/Title.vue';
 import Logo from '../../atoms/Logo.vue';
+import { useStrapi } from '../../../main';
+import { SerieEntity, VideoEntity } from '../../../models/types';
+
+const originals = useStrapi<VideoEntity[]>(['videos', {
+    filters: {
+        original: true
+    },
+    populate: {
+        thumbnail: '*'
+    },
+    sort: 'year:desc'
+}]);
+
+const videos = useStrapi<VideoEntity[]>(['videos', {
+    populate: {
+        thumbnail: '*',
+        series: '*'
+    },
+    sort: 'year:desc',
+    filters: {
+        series: {
+            title: {
+                $null: true
+            }
+        }
+    }
+
+}]);
+
+const series = useStrapi<SerieEntity[]>(['series', {
+    populate: {
+        thumbnail: '*'
+    },
+    sort: 'year:desc'
+}]);
 </script>
 
 <style>
