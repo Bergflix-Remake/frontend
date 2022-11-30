96bdfddd475b74976ba2f609c7c922cc64ec5330
Anniken <61291253+AnnikenYT@users.noreply.github.com>
2022-11-30 00:24:54 +0000

    fix(core): :lipstick: Remove Navbar margin (WIP)

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Navbar from "./stories/organisms/NavBar.vue";
 <template>
   <div>
     <Navbar />
-    <div class="flex flex-col min-h-screen bg-black text-clean-white mt-24">
+    <div class="flex flex-col min-h-screen bg-black text-clean-white">
       <router-view></router-view>
     </div>
     <Footer />
