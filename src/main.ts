902d31d7287f251c407b36729d9bd479dc36005e
Anniken <61291253+AnnikenYT@users.noreply.github.com>
2022-11-30 00:26:32 +0000

    refactor(core): :recycle: Extract API Client to seperate file

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp, reactive, UnwrapNestedRefs } from 'vue';
+import { createApp } from 'vue';
 import App from "./App.vue";
 import "./assets/font.css";
 import "./index.css";
@@ -8,15 +8,9 @@ import { router } from "./router";
 import { store } from "./store";
 import {
 	VueQueryPlugin,
-	useMutation, useQuery, UseQueryOptions, UseQueryReturnType
 } from 'vue-query';
 import "./three-dots.css";
 import { createHead } from '@vueuse/head'
-
-// Strapi Query Client //
-import { ErrorResponse as TError } from './models/custom';
-import Strapi, { StrapiAuthenticationData, StrapiRequestParams, StrapiUser } from 'strapi-sdk-js'
-import { QueryKey } from 'react-query/types/core';
 
 const app = createApp(App);
 const head = createHead();
@@ -30,78 +24,7 @@ app.use(router);
 app.use(store);
 app.use(VueQueryPlugin);
 
-export const strapi = new Strapi({
-	store: {
-		key: "strapi_jwt",
-		useLocalStorage: false,
-		cookieOptions: { path: "/" },
-	},
-	url: "https://api.bergflix.de/",
-	prefix: "/api",
-});
-
-function useStrapiQuery<T>({ queryKey }: any) {
-	return strapi
-		.find<T>(queryKey[0], queryKey[1] ? queryKey[1] : {})
-		.then((res) => {
-			return res.data as T;
-		});
-}
-
-function useStrapiQueryOne<T>({ queryKey }: any) {
-	return strapi
-		.findOne<T>(queryKey[0], queryKey[1], queryKey[2] ? queryKey[2] : {})
-		.then((res) => {
-			return res.data;
-		});
-}
-
-export const strapiLogout = () => {
-	strapi.logout();
-};
-
-export const strapiLogin = () => {
-	return useMutation((authData: StrapiAuthenticationData) =>
-		strapi.login(authData)
-	);
-};
-
-export function useStrapi<T>(
-	queryKey: [string, StrapiRequestParams?],
-	options?: Omit<
-		UseQueryOptions<T, TError, T, QueryKey>,
-		"queryFn" | "queryKey"
-	>
-): UnwrapNestedRefs<UseQueryReturnType<T, TError>> {
-	return reactive(useQuery<T, TError>(queryKey, useStrapiQuery, options));
-}
-
-export function useStrapiOne<T>(
-	queryKey: [string, any, StrapiRequestParams?],
-	options?: Omit<
-		UseQueryOptions<T, TError, T, QueryKey>,
-		"queryFn" | "queryKey"
-	>
-): UnwrapNestedRefs<UseQueryReturnType<T, TError>> {
-	return reactive(useQuery<T, TError>(queryKey, useStrapiQueryOne, options));
-}
-
-export function getUser(): UnwrapNestedRefs<UseQueryReturnType<StrapiUser, TError>> {
-	return reactive(
-		useQuery<StrapiUser, TError>(
-			"user",
-			async () => {
-				const user: StrapiUser = await strapi.fetchUser();
-				if (!user) {
-					throw new Error("User not logged in");
-				}
-				return user;
-			},
-			{
-				retry: false,
-			}
-		)
-	);
-}
+// Strapi Query Export //
+export * from './util/api';
 
 app.mount("#app");
---
+++ src/util/api.ts
@@ -0,0 +1,80 @@
+// Strapi Query Client //
+import { ErrorResponse as TError } from '../models/custom';
+import Strapi, { StrapiAuthenticationData, StrapiRequestParams, StrapiUser } from 'strapi-sdk-js'
+import { QueryKey } from 'react-query/types/core';
+import { useMutation, useQuery, UseQueryOptions, UseQueryReturnType } from 'vue-query';
+import { reactive, UnwrapNestedRefs } from 'vue';
+
+export const strapi = new Strapi({
+	store: {
+		key: "strapi_jwt",
+		useLocalStorage: false,
+		cookieOptions: { path: "/" },
+	},
+	url: "https://api.bergflix.de/",
+	prefix: "/api",
+});
+
+function useStrapiQuery<T>({ queryKey }: any) {
+	return strapi
+		.find<T>(queryKey[0], queryKey[1] ? queryKey[1] : {})
+		.then((res) => {
+			return res.data as T;
+		});
+}
+
+function useStrapiQueryOne<T>({ queryKey }: any) {
+	return strapi
+		.findOne<T>(queryKey[0], queryKey[1], queryKey[2] ? queryKey[2] : {})
+		.then((res) => {
+			return res.data;
+		});
+}
+
+export const strapiLogout = () => {
+	strapi.logout();
+};
+
+export const strapiLogin = () => {
+	return useMutation((authData: StrapiAuthenticationData) =>
+		strapi.login(authData)
+	);
+};
+
+export function useStrapi<T>(
+	queryKey: [string, StrapiRequestParams?],
+	options?: Omit<
+		UseQueryOptions<T, TError, T, QueryKey>,
+		"queryFn" | "queryKey"
+	>
+): UnwrapNestedRefs<UseQueryReturnType<T, TError>> {
+	return reactive(useQuery<T, TError>(queryKey, useStrapiQuery, options));
+}
+
+export function useStrapiOne<T>(
+	queryKey: [string, any, StrapiRequestParams?],
+	options?: Omit<
+		UseQueryOptions<T, TError, T, QueryKey>,
+		"queryFn" | "queryKey"
+	>
+): UnwrapNestedRefs<UseQueryReturnType<T, TError>> {
+	return reactive(useQuery<T, TError>(queryKey, useStrapiQueryOne, options));
+}
+
+export function getUser(): UnwrapNestedRefs<UseQueryReturnType<StrapiUser, TError>> {
+	return reactive(
+		useQuery<StrapiUser, TError>(
+			"user",
+			async () => {
+				const user: StrapiUser = await strapi.fetchUser();
+				if (!user) {
+					throw new Error("User not logged in");
+				}
+				return user;
+			},
+			{
+				retry: false,
+			}
+		)
+	);
+}
