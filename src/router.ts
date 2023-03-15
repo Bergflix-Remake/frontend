ecf5f0f21359f7a2b0025ff2e30ceae56e9539e3
AnnikenYT <anniken@mooonshine.net>
2023-03-15 10:15:03 -0300

    fix(gh): Fixed #145
    Router now always scrolls to the top
    
    Closes #145

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import r from '~pages'
-import { strapi, useResendEmailMutation } from "./main";
+import { strapi } from "./main";
 
 const routes = [
 	...r,
@@ -12,12 +12,16 @@ console.debug(routes)
 export const router = createRouter({
 	history: createWebHistory(),
 	routes,
+	scrollBehavior(to, from, savedPosition) {
+		// always scroll to top
+		return { top: 0 }
+	},
 });
 
 router.beforeEach((to, from) => {
 	if (to.meta.requiresAuth) {
 		console.debug("Route requiresAuth, checking authentication:", strapi.user)
-		if (!strapi.user){
+		if (!strapi.user) {
 			return {
 				name: "login",
 				query: { redirect: to.fullPath, message: "Bitte melde dich an um diese Seite anzusehen." },
