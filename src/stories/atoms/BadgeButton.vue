4166261d8750ac19daa416c7dba29a805939306f
AnnikenYT <anniken@mooonshine.net>
2023-03-23 13:16:37 -0300

    fix: badge button

--- src/stories/atoms/BadgeButton.vue
+++ src/stories/atoms/BadgeButton.vue
@@ -1,7 +1,7 @@
 <template>
   <span
     class="w-12 aspect-square rounded-full p-2 text-white flex flex-col items-center justify-center cursor-pointer shadow-md hover:shadow-xl transition-all hover:scale-105 h-min"
-    :class="`bg-${color}-500 hover:bg-${color||'primary-500'}`"
+    :class="`bg-${color || 'primary'}-500 hover:bg-${color||'primary'}-600`"
     @click="$emit('click')"
   >
     <component :is="icon" v-if="icon" class="w-8 h-8 drop-shadow-xl" />
