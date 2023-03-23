585871de53234d402f5414324c28ec47ab72c6d7
AnnikenYT <anniken@mooonshine.net>
2023-03-23 13:22:40 -0300

    style: format

--- src/stories/organisms/NavBar.vue
+++ src/stories/organisms/NavBar.vue
@@ -1,15 +1,24 @@
 <template>
   <nav
-    class="w-full h-20 fixed  bg-gradient-to-t md:bg-gradient-to-b via-black/50 from-black to-transparent  z-50 md:top-0 md:bottom-0-auto bottom-0 top-auto flex flex-row items-center md:justify-start justify-around md:space-x-2 p-2"
+    class="w-full h-20 fixed bg-gradient-to-t md:bg-gradient-to-b via-black/50 from-black to-transparent z-50 md:top-0 md:bottom-0-auto bottom-0 top-auto flex flex-row items-center md:justify-start justify-around md:space-x-2 p-2"
   >
     <Logo long class="hidden md:block text-3xl" />
     <NavLink v-for="item in navItems" :key="item.name" :name="item.name">
-      <component :is="item.icon" v-if="item.visibleOn.includes('mobile')" class="w-8 h-8 md:hidden" />
-      <span v-if="item.visibleOn.includes('desktop')" class="hidden md:block">{{ item.name }}</span>
+      <component
+        :is="item.icon"
+        v-if="item.visibleOn.includes('mobile')"
+        class="w-8 h-8 md:hidden"
+      />
+      <span v-if="item.visibleOn.includes('desktop')" class="hidden md:block">{{
+        item.name
+      }}</span>
     </NavLink>
     <SearchOverlay class="ml-auto hidden md:block" />
     <!-- Account -->
-    <div class="hidden md:flex h-full relative items-center w-full justify-end" @mouseleave="setDropdown(false, 500)">
+    <div
+      class="hidden md:flex h-full relative items-center w-full justify-end"
+      @mouseleave="setDropdown(false, 500)"
+    >
       <div class="w-14 h-14">
         <Avatar @mouseenter="setDropdown(true)" />
       </div>
@@ -26,11 +35,7 @@
 </template>
 
 <script setup lang="ts">
-import {
-  HomeIcon,
-  SearchIcon,
-  UserIcon,
-} from '@heroicons/vue/solid';
+import { HomeIcon, SearchIcon, UserIcon } from '@heroicons/vue/solid';
 import { ref } from 'vue';
 import Logo from '../atoms/Logo.vue';
 import NavLink from '../atoms/NavLink.vue';
@@ -53,12 +58,12 @@ const navItems = [
   {
     name: 'Home',
     icon: HomeIcon,
-    visibleOn: ['mobile', 'desktop']
+    visibleOn: ['mobile', 'desktop'],
   },
   {
     name: 'Search',
     icon: SearchIcon,
-    visibleOn: ['mobile']
-  }
+    visibleOn: ['mobile'],
+  },
 ];
 </script>
