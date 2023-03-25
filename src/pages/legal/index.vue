f0e681c55ad488257da20d319d9356b626bd8141
Ved Pahune <vedpahune@Veds-MacBook-Air.local>
2023-03-26 00:22:42 +0530

    Added Padding

--- src/pages/legal/index.vue
+++ src/pages/legal/index.vue
@@ -16,27 +16,28 @@ const data = useStrapi<ImprintEntity>(['imprint']);
 </script>
 
 <template>
-  <Title>Rechtliches</Title>
-  <Subtitle id="imprint">Impressum</Subtitle>
-  <!-- eslint-disable-next-line vue/no-v-html -->
-  <section
-  v-if="data.isSuccess"
-  class="p-5 mt-5"
-  v-html="data.data?.attributes?.impressum"
-  />
-  <Subtitle id="privacy">Datenschutz</Subtitle>
-  <!-- eslint-disable-next-line vue/no-v-html -->
-  <section
-    v-if="data.isSuccess"
-    
-    class="p-5 mt-5"
-    v-html="data.data?.attributes?.privacy"
-  />
-  <Subtitle id="terms">Benuzungsbedingungen</Subtitle>
-  <!-- eslint-disable-next-line vue/no-v-html -->
-  <section
-    v-if="data.isSuccess"
-    class="p-5 mt-5"
-    v-html="data.data?.attributes?.terms"
-  />
+  <div class="pt-24">
+    <Title class="pl-6">Rechtliches</Title>
+    <Subtitle class="pl-6" id="imprint">Impressum</Subtitle>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <section
+      v-if="data.isSuccess"
+      class="p-5 mt-5"
+      v-html="data.data?.attributes?.impressum"
+    />
+    <Subtitle id="privacy">Datenschutz</Subtitle>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <section
+      v-if="data.isSuccess"
+      class="p-5 mt-5"
+      v-html="data.data?.attributes?.privacy"
+    />
+    <Subtitle id="terms">Benuzungsbedingungen</Subtitle>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <section
+      v-if="data.isSuccess"
+      class="p-5 mt-5"
+      v-html="data.data?.attributes?.terms"
+    />
+  </div>
 </template>
