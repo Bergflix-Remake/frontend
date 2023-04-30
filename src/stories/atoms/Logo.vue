c91a834fb866a95fadf6231d3f46c176f144ae52
Leo P <anniken@bergflix.de>
2023-04-30 10:55:00 -0300

    feat: slogan meme

--- src/stories/atoms/Logo.vue
+++ src/stories/atoms/Logo.vue
@@ -1,16 +1,22 @@
 <template>
-  <span class="font-bold text-white" @click="$emit('click')">
-    {{ long ? "Bergflix" : "Bf" }}<span class="text-primary-500">. <slot /></span>
+  <span class="font-bold text-white relative" @click="$emit('click')">
+    {{ long ? 'Bergflix' : 'Bf'
+    }}<span class="text-primary-500">. <slot /></span>
+    <a href="https://cdn.upload.systems/uploads/f9kaTj1Q.png"
+      ><span
+        v-if="canHaveSlogan"
+        class="absolute -right-4 -bottom-4 font-inception text-base text-indigo-500 -rotate-12 p-1"
+        >Jawohl!</span
+      ></a
+    >
   </span>
 </template>
-<script setup>
+<script setup lang="ts">
 // eslint-disable-next-line no-undef
-defineProps({
-  long: {
-    type: Boolean,
-    required: false,
-  },
-});
+defineProps<{
+  long?: boolean;
+  canHaveSlogan?: boolean;
+}>();
 // eslint-disable-next-line no-undef
-defineEmits(["click"]);
+defineEmits(['click']);
 </script>
