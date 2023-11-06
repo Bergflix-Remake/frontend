4068f514a27c86c9dae1d5e826844e491c5e7242
Leo P <61291253+AnnikenYT@users.noreply.github.com>
2023-11-06 08:13:18 +0000

    feat: :rewind: remove slogan meme

--- src/stories/atoms/Logo.vue
+++ src/stories/atoms/Logo.vue
@@ -1,24 +1,15 @@
 <template>
   <span class="font-bold text-white relative" @click="$emit('click')">
     {{ long ? 'Bergflix' : 'Bf'
-    }}<span class="text-primary-500">. <slot /></span>
-    <a href="https://cdn.upload.systems/uploads/f9kaTj1Q.png"
-      ><span
-        v-if="canHaveSlogan"
-        class="absolute -right-4 -bottom-4 font-inception text-base text-indigo-500 p-1"
-        ><img
-          src="https://cdn.discordapp.com/emojis/1102957082019364936.webp?size=96&quality=lossless"
-          class="Jawohl"
-          style="width:30px; height:30px"/></span
-      ></a
-    >
+    }}<span class="text-primary-500">.
+      <slot />
+    </span>
   </span>
 </template>
 <script setup lang="ts">
 // eslint-disable-next-line no-undef
 defineProps<{
   long?: boolean;
-  canHaveSlogan?: boolean;
 }>();
 // eslint-disable-next-line no-undef
 defineEmits(['click']);
--- src/stories/organisms/NavBar.vue
+++ src/stories/organisms/NavBar.vue
@@ -2,7 +2,7 @@
   <nav
     class="w-full h-20 fixed bg-gradient-to-t md:bg-gradient-to-b via-black/50 from-black to-transparent z-50 md:top-0 md:bottom-0-auto bottom-0 top-auto flex flex-row items-center md:justify-start justify-around md:space-x-2 p-2"
   >
-    <Logo long can-have-slogan class="hidden md:block text-3xl" />
+    <Logo long class="hidden md:block text-3xl" />
     <NavLink v-for="item in navItems" :key="item.name" :name="item.name">
       <component
         :is="item.icon"
